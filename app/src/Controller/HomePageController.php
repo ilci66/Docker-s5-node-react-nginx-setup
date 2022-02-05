5383ac93d7a3ff4ced94e72834ad9db578b15b48
unknown <akbiyik.ilker7@gmail.com>
2022-02-05 14:39:35 +0100

    rendering very basic homepage

--- app/src/Controller/HomePageController.php
+++ app/src/Controller/HomePageController.php
@@ -5,17 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class HomePageController extends AbstractController
 {
     /**
-     * @Route("/home/page", name="home_page")
+     * @Route("/", name="home_page")
      */
     public function index(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/HomePageController.php',
-        ]);
+        // return $this->json([
+        //     'message' => 'Welcome to your new controller!',
+        //     'path' => 'src/Controller/HomePageController.php',
+        // ]);
+        return $this->render('base.html.twig');
     }
 }
